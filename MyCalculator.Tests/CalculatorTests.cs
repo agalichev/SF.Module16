5714f9daefbac1c59ad37186c652769f68ba4e31
Alexander Galichev <lex.galichev@gmail.com>
2023-03-18 21:35:25 +1000

    Added MyCalculator.Tests.csproj library with CalculatorTests.cs class.

---
+++ MyCalculator.Tests/CalculatorTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace MyCalculator.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AdditionalMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Additional(11, 23) == 34);
+        }
+
+        [Test]
+        public void SubtractionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Subtraction(8, 2) == 6);
+        }
+
+        [Test]
+        public void MultiplicationMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Multiplication(6, 6) == 36);
+        }
+
+        [Test]
+        public void DivisionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Division(49, 7) == 7);
+        }
+    }
+}
--- MyCalculator/Calculator.cs
+++ MyCalculator/Calculator.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace MyCalculator
 {
-    internal class Calculator
+    public class Calculator
     {
+        public int Additional(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
     }
 }
